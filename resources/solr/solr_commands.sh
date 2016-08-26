255525f5de9091abbbfc48072f57883527c5cd90
Peyton Casper <peyton@PPC-Ubuntu-Desktop.attlocal.net>
2016-08-26 17:06:50 -0500

    Updated the solr config and adjusted the solr script

---
+++ resources/solr/solr_commands.sh
@@ -0,0 +1,6 @@
+dsetool unload_core vehicle_tracking_app.current_location
+dsetool create_core vehicle_tracking_app.current_location reindex=true schema=resources/solr/geo.xml solrconfig=resources/solr/solrconfig.xml
+dsetool unload_core vehicle_tracking_app.vehicle_stats
+dsetool create_core vehicle_tracking_app.vehicle_stats reindex=true schema=resources/solr/geo_vehicle.xml solrconfig=resources/solr/solrconfig.xml
+dsetool unload_core vehicle_tracking_app.vehicle_events
+dsetool create_core vehicle_tracking_app.vehicle_events reindex=true schema=resources/solr/events.xml solrconfig=resources/solr/solrconfig.xml
