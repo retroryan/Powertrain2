514736f5ce08e9935deee4e559126862031c3357
Peyton Casper <peyton@PPC-Laptop.attlocal.net>
2016-08-30 17:39:53 -0500

    Added another finish event

--- public/game/bkcore/hexgl/Gameplay.js
+++ public/game/bkcore/hexgl/Gameplay.js
@@ -73,15 +73,18 @@ bkcore.hexgl.Gameplay = function(opts)
 
 			if(self.lap == this.maxLaps)
 			{
+				self.vehicleStream.sendEvent("finish", self.lap);
 				self.end(self.results.FINISH);
+
 			}
 			else
             {
-                self.vehicleStream.sendEvent("lap", self.lap);
                 self.lap++;
 				self.hud != null && self.hud.updateLap(self.lap, self.maxLaps);
 				if(self.lap == self.maxLaps)
 					self.hud != null && self.hud.display("Final lap", 0.5);
+				self.vehicleStream.sendEvent("lap", self.lap);
+
 			}
 		}
 		else if(cp != -1 && cp != self.previousCheckPoint)
